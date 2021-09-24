1cc7435c4647a25e6d499affe1ac015a6547faa4
Masih Javidan <89029958+MasihJavidan@users.noreply.github.com>
2021-09-24 21:07:13 +0330

    Update settings.py

--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -147,8 +147,8 @@ DEFAULT_FROM_EMAL = 'masih.javidan69@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'masih.javidan69@gmail.com'
-EMAIL_HOST_PASSWORD = 'Masih55416619'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'pass'
 
 # ckeditor
 
